f9a426887a2af59cba78c98b54dba64e3acd6a5e
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-09-16 12:14:26 -0500

    se crean modelos de datos

---
+++ src/main/java/com/example/SURATECH5/MODELOS/HistoriaClinica.java
@@ -0,0 +1,9 @@
+package com.example.SURATECH5.MODELOS;
+
+public class HistoriaClinica {
+
+    String descripcion;
+    Integer diagnosticos;
+
+
+}
---
+++ src/main/java/com/example/SURATECH5/MODELOS/SignoVital.java
@@ -0,0 +1,12 @@
+package com.example.SURATECH5.MODELOS;
+
+import java.time.LocalDate;
+
+public class SignoVital {
+    LocalDate
+    String nombre;
+    String valor;
+    String fechaTomo ;
+
+
+}
